69b8a0da917b6864c3883eb4e1a0401119d302e7
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-07-21 14:36:59 -0500

    add pretty colors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 import Favorites from '../Favorites/Favorites';
 import Search from '../Search/Search';
 import FavoritesList from '../FavoritesList/FavoritesList';
-import {HashRouter as Router, Route, Link} from 'react-router-dom';
-
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 function App(props) {
-
-
-  return (
-    <Router>
-    <div>
-      <h1>Giphy Search!</h1>
-      <Search />
-      <Route path="/favorite" exact>
-      <Favorites />
-      </Route>
-    </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<h1>Giphy Search!</h1>
+				<Route path='/' exact>
+					<Search />
+				</Route>
+				<Route path='/favorite' exact>
+					<Favorites />
+				</Route>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
