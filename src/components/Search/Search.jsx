443d00e811ba409fbfe4999a94664d508757d0fd
Tim Miner <poeminminer1@gmail.com>
2022-07-21 09:52:27 -0500

    working search/get request

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
 function Search() {
+    // Store to get gifs we searched
+    const gifList = useSelector(store => store.gifList);
+    // local state to hold input variable
+    const [newGif, setNewGif] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(newGif);
+        // get request
+        dispatch({
+            type: 'GIF_SELECT',
+            payload: newGif
+        });
+
+        setNewGif('');
+    }
+
+    const dispatch = useDispatch();
+
 
     return (
-        console.log('ADD HERE')
+        <>
+            <form onSubmit={handleSubmit}>
+                <label name='gifSearch'>What gif would you like search</label>
+                <input value={newGif} name="gifSearch" type="text" onChange={(event) => setNewGif(event.target.value)} />
+                <button type="submit">Submit</button>
+            </form>
+            <h2>HERE are your search results:</h2>
+            {
+                // image is from gifResponse.data.data in the index.js. Already looked through the data.data. 
+                // Later looks through assumed(data.data).images.original.url. Path can be seen on browser object.
+                gifList.map((image, i) => {
+                return <img key={i} src={image.images.original.url} />
+                })
+            }
+
+        </>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 // imported by Tim
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+// import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
@@ -12,19 +12,48 @@ import { takeEvery, put } from 'redux-saga/effects';
 import axios from 'axios';
 import createSagaMiddleware from 'redux-saga';
 
+// Generator Function for getSearch with sagas
+function* getSearch(action) {
+    // action.payload is what we put in the search bar. sent as template literal.
+    console.log(action.payload);
+    try {
+        const gifResponse = yield axios.get(`/api/category/search/${action.payload}`)
+        yield console.log('RESPONSE HERE', gifResponse);
+        yield put({ type: 'GET_GIFS', payload: gifResponse.data.data })
+        // gifResult is from the variable made above this line. 
+        // 'GET_GIFS has to match what is in the reducer
+        // yield console.log('RESULT FROM DB', gifResult.data);
+        // yield put({ type: 'GET_GIFS', payload: gifResult.data })
+    }
+    catch (error) {
+        console.log('ERROR in getSearch in index.js:', error);
+    }
+}
+
 function* watchSaga() {
     // yield something
+    // action type 'GIF_SELECT' has to match what is in the Search.jsx
+    yield takeEvery('GIF_SELECT', getSearch);
 }
 
 const sagaMiddleware = createSagaMiddleware();
 
 // reducers
+const gifList = (state = [], action) => {
+    switch (action.type) {
+        case 'GET_GIFS':
+            console.log('ACTION.PAYLOAD gifList', action.payload);
+            return action.payload;
+        default:
+            return state;
+    }
+}
 
 
 // Create one store that all components can use
 const storeInstance = createStore(
     combineReducers({
-        basketReducer,
+        gifList,
     }),
     // Add sagaMiddleware to our store
     applyMiddleware(sagaMiddleware, logger),
@@ -33,4 +62,4 @@ const storeInstance = createStore(
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(watchSaga);
 
-ReactDOM.render(<Provider store= {storeInstance}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
